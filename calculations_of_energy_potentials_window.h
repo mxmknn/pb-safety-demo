4e3c4d331584bdf9b3711d91e30ff339292d044f
mxmknn <mxmknn@MacBook-Pro-mxmknn.local>
2026-05-01 23:11:16 +0300

    Fix-25 debug version

--- calculations_of_energy_potentials_window.h
+++ calculations_of_energy_potentials_window.h
@@ -1,36 +1,3 @@
-// #ifndef CALCULATIONS_OF_ENERGY_POTENTIALS_WINDOW_H
-// #define CALCULATIONS_OF_ENERGY_POTENTIALS_WINDOW_H
-
-// #include <QMainWindow>
-
-// class QTableWidget;
-// class QPushButton;
-
-// class Calculations_Energy_window : public QMainWindow
-// {
-//     Q_OBJECT
-
-// public:
-//     explicit Calculations_Energy_window(QWidget *parent = nullptr);
-//     ~Calculations_Energy_window() override = default;
-
-// private:
-//     QTableWidget *unitsTable = nullptr;
-//     QTableWidget *blocksTable = nullptr;
-
-//     QPushButton *backButton = nullptr;
-//     QPushButton *closeButton = nullptr;
-
-//     void setupUi();
-
-//     QTableWidget *createUnitsTable();
-//     QTableWidget *createBlocksTable();
-
-//     void moveSelectedRow(QTableWidget *table, int direction);
-// };
-
-// #endif
-
 #ifndef CALCULATIONS_OF_ENERGY_POTENTIALS_WINDOW_H
 #define CALCULATIONS_OF_ENERGY_POTENTIALS_WINDOW_H
 
