ee7e44445344862389d151ffcd1364c130234cb6
mxmknn <mxmknn@MacBook-Pro-mxmknn.local>
2026-05-01 23:29:19 +0300

    Fix-29

--- calculations_of_energy_potentials_window.cpp
+++ calculations_of_energy_potentials_window.cpp
@@ -47,4 +47,40 @@ void Calculations_Energy_window::buildInterface()
     mainLayout->addWidget(closeButton);
 
     connect(closeButton, &QPushButton::clicked, this, &Calculations_Energy_window::close);
+
+    QLabel *blocksLabel = new QLabel("Блоки :", this);
+
+    QTableWidget *blocksTable = new QTableWidget(this);
+    blocksTable->setColumnCount(9);
+    blocksTable->setRowCount(1);
+
+    blocksTable->setHorizontalHeaderLabels({
+        "Название блока",
+        "Коэф.\nуч. во\nвзрыве",
+        "Токсичные\nв-ва",
+        "Категория",
+        "Полный\nпотенциал",
+        "Приведенная\nмасса",
+        "Относительный\nпотенциал",
+        "Радиус\nRo",
+        "Оценка\nзон"
+    });
+
+    blocksTable->setItem(0, 0, new QTableWidgetItem("Аппарат с газовой фазой"));
+    blocksTable->setItem(0, 1, new QTableWidgetItem("0,1"));
+    blocksTable->setItem(0, 2, new QTableWidgetItem("Нет"));
+    blocksTable->setItem(0, 3, new QTableWidgetItem("2"));
+    blocksTable->setItem(0, 4, new QTableWidgetItem("1,28E+08"));
+    blocksTable->setItem(0, 5, new QTableWidgetItem("2,78E+03"));
+    blocksTable->setItem(0, 6, new QTableWidgetItem("30,49"));
+    blocksTable->setItem(0, 7, new QTableWidgetItem("7,741"));
+    blocksTable->setItem(0, 8, new QTableWidgetItem("Да"));
+
+    blocksTable->verticalHeader()->setVisible(false);
+    blocksTable->setSelectionBehavior(QAbstractItemView::SelectRows);
+    blocksTable->setSelectionMode(QAbstractItemView::SingleSelection);
+    blocksTable->setEditTriggers(QAbstractItemView::NoEditTriggers);
+
+    mainLayout->addWidget(blocksLabel);
+    mainLayout->addWidget(blocksTable);
 }
